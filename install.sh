6750aab7b1ebc1f83af7d580937183b674e0845c
JNCoder <nwyfss@gmail.com>
2020-09-11 09:51:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,18 +8,12 @@ input_var(){
     echo "应用名称：${IBM_APP_NAME}"
     read -p "请输入你的应用内存大小(默认256)：" IBM_MEM_SIZE
     if [ -z "${IBM_MEM_SIZE}" ];then
-        IBM_MEM_SIZE=256
+    IBM_MEM_SIZE=256
     fi
 }
 
 create_mainfest_file(){
     echo "进行配置。。。"
-    read -p "请输入你的应用名称：" IBM_APP_NAME
-    echo "应用名称：${IBM_APP_NAME}"
-    read -p "请输入你的应用内存大小(默认256)：" IBM_MEM_SIZE
-    if [ -z "${IBM_MEM_SIZE}" ];then
-    IBM_MEM_SIZE=256
-    fi
     echo "内存大小：${IBM_MEM_SIZE}"
     UUID=$(cat /proc/sys/kernel/random/uuid)
     echo "生成随机UUID：${UUID}"
